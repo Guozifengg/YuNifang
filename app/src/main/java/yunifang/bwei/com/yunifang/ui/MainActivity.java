504c5419c7a10a499f2fe894519fdbfb5657676f
guozifeng <961331126@qq.com>
2017-03-16 11:22:44 +0800

    建包

---
+++ app/src/main/java/yunifang/bwei/com/yunifang/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package yunifang.bwei.com.yunifang.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import yunifang.bwei.com.yunifang.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
